65a562880e03bf56b7db97f2e84992c07c464a8b
Lana <lanawo370@gmail.com>
2025-01-26 12:05:20 -0800

    code runs without errors -- got basic structure down

--- robot_temp_control.py
+++ robot_temp_control.py
@@ -1,6 +1,6 @@
 class Fan:
-    def __int__(self, max_fan_rpm):
-        self.__max_rpm = max_fan_rpm
+    def __init__(self, max_fan_rpm):
+        self.max_rpm = max_fan_rpm
         self.__curr_speed = 0
     
     def set_speed(self, new_speed):
@@ -20,23 +20,26 @@ class Subsystem:
         return self.__curr_temperature
     
 class Robot():
-    def __init__(self, num_subsystems, fans_present):
-        self.subsystems = [Subsystem()] * num_subsystems
+    def __init__(self, num_subsystems, num_fans):
+        self.subsystems = [Subsystem(0.0)] * num_subsystems
         self.fans = []
         
         for i in range(num_fans):
             # need error checking for fan max speed
-            max_speed = int(input(f"What is the max speed of fan {i + 1}? "))
-            fans.append(Fan(max_speed))
-            
+            max_speed = check_valid_int_input(f"What is the max speed of fan {i + 1}? ")
+            self.fans.append(Fan(max_speed))
+        
+        self.update_subsystem_temperatures()
+        
+    def update_subsystem_temperatures(self):
         for i, subsystem in enumerate(self.subsystems):
-            temp = int(input(f"What is the current temperature of subsystem {i + 1}? "))
+            temp = check_valid_int_input(f"What is the current temperature of subsystem {i + 1}? ")
             subsystem.set_temperature(temp)
-            max_temp = max(max_temp, temp)
-            update_fan_percent_max_rpm(max_temp)
+            max_temp = max([subsystem.get_temperature() for subsystem in self.subsystems])
+            self.__update_fan_percent_max_rpm(max_temp)
 
-    def update_fan_percent_max_rpm(self, curr_temps_max):
-        fan_speed_percent = float(0);
+    def __update_fan_percent_max_rpm(self, curr_temps_max):
+        fan_speed_percent = float(0)
 
         if curr_temps_max <= 25:
             fan_speed_percent = 0.2
@@ -47,29 +50,28 @@ class Robot():
         
         for fan in self.fans:
             fan.set_speed(fan_speed_percent * fan.max_rpm)
-
-        #iterate through list of fan objects with set_fan_speed_percent
     
 ### Put these functions in own file###
-def get_valid_input(prompt):
+def check_valid_int_input(prompt):
     while True:
         try:
             user_input = int(input(prompt))
             return user_input
         except ValueError:
-            print(f"\nInput not valid. Please enter a number.")
+            print(f"\nInput not valid. Please enter a number. ")
         except EOFError:
-            print(f"\nNo input given. Please enter a number.")
+            print(f"\nNo input given. Please enter a number. ")
 #######################################
             
-num_subsystems = get_valid_input("How many subsystems are there?")
-fans_present = get_valid_input("How many fans are there?")
+num_subsystems = check_valid_int_input("How many subsystems are there? ")
+fans_present = check_valid_int_input("How many fans are there? ")
 robot = Robot(num_subsystems, fans_present)
 
+
+
+
+# IDEAS:
 # give it some inital temp, then based on current temp, then change the temp to act accordingly. 
-
-
-
     
 # def update_dashboard()
 #     # landing page
